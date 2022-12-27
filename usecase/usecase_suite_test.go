32552c3dc0c74aa225e8e802082286799d699179
realtmq <quangtm.bkhn@gmail.com>
2022-12-27 11:06:04 +0700

    add book usecase unitest

---
+++ usecase/usecase_suite_test.go
@@ -0,0 +1,43 @@
+package usecase_test
+
+import (
+	"peanut/usecase"
+	"testing"
+
+	"peanut/repository/mock"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var bookRepo *mock.MockBookRepo
+var bookUc usecase.BookUsecase
+
+func TestUsecase(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Usecase Suite")
+}
+
+var _ = BeforeSuite(func() {
+	sqlDB, smock, _ := sqlmock.New()
+	defer sqlDB.Close()
+
+	// dsn := "root:9899@tcp(localhost:3307)/peanut_db?charset=utf8mb4&parseTime=True&loc=Local"
+	// sqlDB, _ := sql.Open("mysql", dsn)
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		Conn:                      sqlDB,
+		SkipInitializeWithVersion: true, // auto configure based on currently MySQL version
+	}), &gorm.Config{})
+	Expect(err).To(BeNil())
+	Expect(smock).NotTo(BeNil())
+	Expect(db).NotTo(BeNil())
+
+	ctrl := gomock.NewController(GinkgoT())
+	defer ctrl.Finish()
+	bookRepo = mock.NewMockBookRepo(ctrl)
+	bookUc = usecase.NewBookUsecase(bookRepo)
+})
